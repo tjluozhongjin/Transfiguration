ebef82ae0205fdedb077aaf754bf194d5e58c67c
Yang Li <zjzsliyang@gmail.com>
2017-06-13 21:46:43 +0800

    feat(all): bugs fixed & add agenda

--- DoorBell/DoorBell/AgendaViewController.swift
+++ DoorBell/DoorBell/AgendaViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import EventKit
+import EventKitUI
 
 class AgendaViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationController?.setNavigationBarHidden(false, animated: true)
-
     
+    getAgenda()
+  }
+  
+  func getAgenda() {
+    let eventStore = EKEventStore()
+    eventStore.requestAccess(to: .event) { (granted, error) in
+      if (granted) && (error == nil) {
+        let startDate = Date().addingTimeInterval(-3600 * 24 * 1)
+        let endDate = Date().addingTimeInterval(3600 * 24 * 7)
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        if let event = eventStore.events(matching: predicate) as [EKEvent]! {
+          for iter in event {
+            print("title \(iter.title)")
+            print("start \(iter.startDate)")
+            print("end \(iter.endDate)")
+          }
+        }
+      }
+    }
   }
   
   override func didReceiveMemoryWarning() {
--- DoorBell/DoorBell/VisitorViewController.swift
+++ DoorBell/DoorBell/VisitorViewController.swift
@@ -21,29 +21,30 @@ class VisitorViewController: UIViewController {
     
     visitorView.scalesPageToFit = true
     visitorView.contentMode = .scaleAspectFit
-    visitorView.loadRequest(URLRequest(url: URL(string: "https://demo100.chinacloudsites.cn/getFile")!))
+    visitorView.loadRequest(URLRequest(url: URL(string: "http://60.205.206.174:3000/getFile")!))
     let now = Date()
     let dformatter = DateFormatter()
     dformatter.dateFormat = "HH:mm"
     currentTime.text = String(describing: dformatter.string(from: now))
     
-    Alamofire.request("https://demo100.chinacloudsites.cn/getData", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseString { (response) in
-      let responseData = response.result.value!
-      let responseJson = JSON(data: responseData.data(using: String.Encoding.utf8)!)
-      
-      let similarity  = UILabel(frame: CGRect(x: 204, y: 392, width: 100, height: 21))
-      similarity.text = responseJson["confidence"].stringValue
-      similarity.textColor = UIColor.darkGray
-      self.view.addSubview(similarity)
-      
-      let similarityData: Double = Double(responseJson["confidence"].stringValue)!
-      if similarityData < 0.7 {
-        let alertController = UIAlertController(title: "Hola", message: "Having a new Stranger!", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
-        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil)
-        alertController.addAction(cancelAction)
-        alertController.addAction(okAction)
-        self.present(alertController, animated: true, completion: nil)
+    Alamofire.request("http://60.205.206.174:3000/getData", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseString { (response) in
+      if let responseData = response.result.value {
+        let responseJson = JSON(data: responseData.data(using: String.Encoding.utf8)!)
+        
+        let similarity  = UILabel(frame: CGRect(x: 204, y: 392, width: 100, height: 21))
+        similarity.text = responseJson["confidence"].stringValue
+        similarity.textColor = UIColor.darkGray
+        self.view.addSubview(similarity)
+        
+        let similarityData: Double = Double(responseJson["confidence"].stringValue)!
+        if similarityData < 0.7 {
+          let alertController = UIAlertController(title: "Hola", message: "Having a new Stranger!", preferredStyle: .alert)
+          let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+          let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil)
+          alertController.addAction(cancelAction)
+          alertController.addAction(okAction)
+          self.present(alertController, animated: true, completion: nil)
+        }
       }
     }
   }
